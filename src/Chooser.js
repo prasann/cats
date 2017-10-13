a52e6151b9fb2c12d8bcba67d5478f1cbca0d6f5
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2017-10-13 22:35:42 +0200

    Add cat chooser

--- src/Chooser.js
+++ src/Chooser.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 class Chooser extends Component {
   render() {
-    return <div>Chooser</div>;
+    return <div>
+      <input type="image" src="https://octodex.github.com/images/jetpacktocat.png" />
+      <input type="image" src="https://octodex.github.com/images/filmtocat.png" />
+      <input type="image" src="https://octodex.github.com/images/mountietocat.png" />
+      <input type="image" src="https://octodex.github.com/images/saritocat.png" />
+      <input type="image" src="https://octodex.github.com/images/steroidtocat.png" />
+      <input type="image" src="https://octodex.github.com/images/dunetocat.png" />
+      <input type="image" src="https://octodex.github.com/images/minertocat.png" />
+      <input type="image" src="https://octodex.github.com/images/catstello.png" />
+      <input type="image" src="https://octodex.github.com/images/saketocat.png" />
+      <input type="image" src="https://octodex.github.com/images/yaktocat.png" />
+    </div>;
   }
 }
 
--- src/Viewer.js
+++ src/Viewer.js
@@ -3,14 +3,11 @@ import React, { Component } from 'react';
 class Viewer extends Component {
   render() {
     return <div>
-      <svg>
+      <svg width="660" height="500" >
         <image
           width="100%" height="100%"
           xlinkHref="https://octodex.github.com/images/jetpacktocat.png" />
       </svg>
-      <div>
-        <span> Source for Octocats: https://octodex.github.com/</span>
-      </div>
     </div>;
   }
 }
