274b4a466839dabcf1ec92294cf008dcabec5f41
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2017-10-13 22:38:01 +0200

    Refactored cat chooser

--- src/Chooser.js
+++ src/Chooser.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 
+const CatChooser = () => {
+  return <div>
+    <input type="image" src="https://octodex.github.com/images/jetpacktocat.png" />
+    <input type="image" src="https://octodex.github.com/images/filmtocat.png" />
+    <input type="image" src="https://octodex.github.com/images/mountietocat.png" />
+    <input type="image" src="https://octodex.github.com/images/saritocat.png" />
+    <input type="image" src="https://octodex.github.com/images/steroidtocat.png" />
+    <input type="image" src="https://octodex.github.com/images/dunetocat.png" />
+    <input type="image" src="https://octodex.github.com/images/minertocat.png" />
+    <input type="image" src="https://octodex.github.com/images/catstello.png" />
+    <input type="image" src="https://octodex.github.com/images/saketocat.png" />
+    <input type="image" src="https://octodex.github.com/images/yaktocat.png" />
+  </div>;
+};
+
 class Chooser extends Component {
   render() {
-    return <div>
-      <input type="image" src="https://octodex.github.com/images/jetpacktocat.png" />
-      <input type="image" src="https://octodex.github.com/images/filmtocat.png" />
-      <input type="image" src="https://octodex.github.com/images/mountietocat.png" />
-      <input type="image" src="https://octodex.github.com/images/saritocat.png" />
-      <input type="image" src="https://octodex.github.com/images/steroidtocat.png" />
-      <input type="image" src="https://octodex.github.com/images/dunetocat.png" />
-      <input type="image" src="https://octodex.github.com/images/minertocat.png" />
-      <input type="image" src="https://octodex.github.com/images/catstello.png" />
-      <input type="image" src="https://octodex.github.com/images/saketocat.png" />
-      <input type="image" src="https://octodex.github.com/images/yaktocat.png" />
-    </div>;
+    return <CatChooser/>;
   }
 }
 
