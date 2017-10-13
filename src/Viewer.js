6351fbf36750ca5d764a44b7c2ae5de76b1b6e8c
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2017-10-13 22:21:11 +0200

    Added the first octocat

--- src/Viewer.js
+++ src/Viewer.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 
 class Viewer extends Component {
   render() {
-    return <div>Viewer</div>;
+    return <div>
+      <svg>
+        <image
+          width="100%" height="100%"
+          xlinkHref="https://octodex.github.com/images/jetpacktocat.png" />
+      </svg>
+      <div>
+        <span> Source for Octocats: https://octodex.github.com/</span>
+      </div>
+    </div>;
   }
 }
 
